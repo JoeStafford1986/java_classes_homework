e31783ada937f477b125a368f318898e24713b1f
Joe Stafford <joestafford1986@gmail.com>
2018-05-15 16:59:48 +0100

    initial commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> bookCollection;
+
+    public Library() {
+        this.bookCollection = new ArrayList();
+    }
+
+    public int bookCount() {
+        return this.bookCollection.size();
+    }
+}
